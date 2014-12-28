0bb6c44fef5a66403e0e2d6a4a23f9e31f759084
Gregor Meyenberg <gregor@meyenberg.de>
2014-12-28 12:19:05 +0100

    clean up

--- ops.erl
+++
@@ -1,4 +0,0 @@
--module(ops).
--export([add_a_value/2]).
- 
-add_a_value(A,B) -> A + B.
--- ops_tests.erl
+++
@@ -1,5 +0,0 @@
--module(ops_tests).
--include_lib("eunit/include/eunit.hrl").
- 
-add_a_value_test() ->
-4 = ops:add_a_value(2,2).
