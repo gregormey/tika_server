c730b27928b5d721cc3511801531366c4de4581f
Gregor Meyenberg <gregor@meyenberg.de>
2015-12-20 16:55:37 +0100

    adding metrics page

--- priv/var/www/js/app.js
+++ priv/var/www/js/app.js
@@ -10,9 +10,52 @@ angular.module('verificationApp', ['ngRoute'])
     	}
     });
 })
+.controller('MetricsCtrl', function($scope,$routeParams,$http){
+  
+  var draw = function(){
+
+    google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+  };
+
+
+  $http.get('/user').then(function successCallback(response) {
+    if(response.status==200)
+    {
+     
+    }
+  });
+  $http.get('/event').then(function successCallback(response) {
+    if(response.status==200)
+    {
+     
+    }
+  });
+  draw();
+})
 .config(function($routeProvider) {
   $routeProvider
   	.when('/', { templateUrl: 'templates/welcome.html' })
+    .when('/metrics', { templateUrl: 'templates/metrics.html', 
+                        controller: 'MetricsCtrl' })
     .when('/verify/:code', { templateUrl: 'templates/verification.html',
     						 controller: 'VerificationCtrl'
     					 })
